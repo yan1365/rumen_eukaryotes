d3d879809260c49a3ef6454e9e53dfbecd966620
yan1365 <yan.1365@osu.edu>
2024-01-01 09:39:31 -0500

    add functions to generate the final report and fasta

--- GutEuk/GutEuk.py
+++ GutEuk/GutEuk.py
@@ -66,6 +66,11 @@ parser.add_argument(
     "-t", "--threads", help="Number of threads used. The default is 1.", type=int, default=1
 )
 
+
+parser.add_argument(
+    "--to_fasta", help="Write predicted results to fasta files", action='store_true'
+)
+
 args = parser.parse_args()
 
 def main():
@@ -77,6 +82,8 @@ def main():
     fasta_filename = os.path.basename(args.input)
     output_dir = os.path.normpath(args.output_dir)
     min_length = args.min_len
+    to_fasta = args.to_fasta
+
     if min_length > 5000:
         min_length = 5000
     threads = args.threads
@@ -131,37 +138,126 @@ def main():
         ## the resultant arrays could be used for prediction
         utils.save_npz_parellel(tmp_dir, threads)
 
-
+    # prediction for inidividual fragment
     def prediction():
         indexs = [f.split("forward_")[1].split(".npz")[0] for f in glob.glob(f"{tmp_dir}/forward*.npz")]
         for index in indexs:
             utils.predict(tmp_dir, index)
 
+
+    # organize fragment prediction results and generate final output
+    def generate_final_output():
+        seqorigin = glob.glob(f"{tmp_dir}/seqorigin*.csv")
+        stage1_res = glob.glob(f"{tmp_dir}/input_fasta_*_stage1_out.csv")
+        stage2_res = glob.glob(f"{tmp_dir}/input_fasta_*_stage2_out.csv")
+        if len(seqorigin) == 1:
+            seqorigin_df = pd.read_csv(seqorigin[0])
+            stage1_df = pd.read_csv(stage1_res[0])
+            stage2_df = pd.read_csv(stage2_res[0])
+
+        else:
+            seqorigin_df = pd.concat([pd.read_csv(f) for f in seqorigin])
+            stage1_df = pd.concat([pd.read_csv(f) for f in stage1_res])
+            stage2_df = pd.concat([pd.read_csv(f) for f in stage2_res])
+
+        stage1_res = pd.merge(seqorigin_df, stage1_df, on = "seq")
+        stage2_res = pd.merge(seqorigin_df, stage2_df, on = "seq")
+
+        stage1_final_prediction = {}
+        for f in set(stage1_res.origin):
+            df = stage1_res.query('origin == @f')
+            # if the input sequence unfragmented (seq length < 10,000 bp)
+            if len(df) == 1:
+                prediction = list(df.predict)[0]
+                stage1_final_prediction[f] = prediction
+            # if the input sequence fragmented, the final prediction for the original sequence determined based on the majority rule
+            else:
+                prediction = list(df.predict)
+                prediction_pro = prediction.count("prokaryotes")
+                prediction_euk = prediction.count("eukaryotes") 
+                if prediction_pro > prediction_euk:
+                    stage1_final_prediction[f] = "prokaryotes"
+                elif prediction_pro < prediction_euk:
+                    stage1_final_prediction[f] = "eukaryotes"
+                else:
+                    stage1_final_prediction[f] = "undetermined"
+
+        stage2_final_prediction = {}
+        for f in set(stage2_res.origin):
+            df = stage2_res.query('origin == @f')
+            if len(df) == 1:
+                prediction = list(df.predict)[0]
+                stage2_final_prediction[f] = prediction
+            else:
+                prediction = list(df.predict)
+                prediction_fungi = prediction.count("fungi")
+                prediction_protozoa = prediction.count("protozoa") 
+                if prediction_fungi > prediction_protozoa:
+                    stage2_final_prediction[f] = "fungi"
+                elif prediction_fungi < prediction_protozoa:
+                    stage2_final_prediction[f] = "protozoa"
+                else:
+                    stage2_final_prediction[f] = "undetermined"
+        
+        stage1_final_prediction_df = pd.DataFrame.from_dict(stage1_final_prediction, orient = "index").rename(columns = {0:"stage1_prediction"})
+        stage2_final_prediction_df = pd.DataFrame.from_dict(stage2_final_prediction, orient = "index").rename(columns = {0:"stage2_prediction"})
+        final_output = pd.merge(stage1_final_prediction_df, stage2_final_prediction_df, left_index = True, right_index = True, how="left")
+        final_output.reset_index(names = "sequence_id", inplace = True)
+        final_output.to_csv(f"{output_dir}/{fasta_filename.split('.')[0]}_EukRep_output.csv", index = None)
+
+        return final_output
+
+    
+    def write_to_fasta(final_output):
+        prokaryotes = list(final_output.query('stage1_prediction == "prokaryotes"').sequence_id)
+        eukaryotes = list(final_output.query('stage1_prediction == "eukaryotes"').sequence_id)
+        protozoa = list(final_output.query('stage2_prediction == "protozoa"').sequence_id)
+        fungi = list(final_output.query('stage2_prediction == "fungi"').sequence_id)
+
+        with open(f"{output_dir}/{fasta_filename.split('.')[0]}_EukRep_prokaryotes.fasta", "w") as prokaryotes_out:
+            with open(f"{output_dir}/{fasta_filename.split('.')[0]}_EukRep_eukaryotes.fasta", "w") as eukaryotes_out:
+                with open(f"{output_dir}/{fasta_filename.split('.')[0]}_EukRep_protozoa.fasta", "w") as protozoa_out:
+                    with open(f"{output_dir}/{fasta_filename.split('.')[0]}_EukRep_fungi.fasta", "w") as fungi_out:
+                        records = SeqIO.parse(f"{input_fasta}", "fasta")
+                        for record in records:
+                            if record.id in prokaryotes:
+                                SeqIO.write(record, prokaryotes_out, "fasta") 
+                            elif record.id in eukaryotes:
+                                SeqIO.write(record, eukaryotes_out, "fasta")
+                                if record.id in protozoa:
+                                     SeqIO.write(record, protozoa_out, "fasta")
+                                elif record.id in fungi:
+                                     SeqIO.write(record, fungi_out, "fasta")
+
+
+
     preprocessing_start = time.time()
     preprocessing(input_fasta, tmp_dir, threads, min_length)
     preprocessing_end = time.time()
-    logging.info(f"Preprocessing finished in {preprocessing_end - preprocessing_start} secs")
+    logging.info(f"Preprocessing finished in {preprocessing_end - preprocessing_start:.2f} secs")
     
 
     prediction_start = time.time()
     prediction()
+    final_output = generate_final_output()
+    if to_fasta:
+        write_to_fasta(final_output)
     prediction_end = time.time()
-    logging.info(f"Prediction finished in {prediction_end - prediction_start} secs")
-  
-    # # clearn up, remove tmp dir
-    # try:
-    #     shutil.rmtree(f"{tmp_dir}")
-    #     print(f"Tmp directory '{tmp_dir}' and its contents removed successfully.")
-    # except Exception as e:
-    #     print(f"Error removing directory '{tmp_dir}': {e}")
+    logging.info(f"Prediction finished in {prediction_end - prediction_start:.2f} secs")
+
+
+    # clearn up, remove tmp dir
+    try:
+        shutil.rmtree(f"{tmp_dir}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
     end_time = str(datetime.now()).split('.')[0]
     time_spent_end = time.time()
 
     logging.info(f"Start: {start_time}")
     logging.info(f"End: {end_time}")
-    logging.info(f"Time spent: {time_spent_end - time_spent_start}")
-
+    logging.info(f"Time spent: {time_spent_end - time_spent_start:.2f}")
 
 
 if __name__ == "__main__":
